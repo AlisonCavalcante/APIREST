115fedef7c85719f89bf4d0d0ae2bcb9065f550c
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-04-29 10:33:08 -0300

    add dotenv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // import
+require('dotenv').config();
 const express = require("express");
 // inicializando o express em forma de função
 const app = express();
@@ -26,10 +27,13 @@ app.get("/", (requisicao, response) => {
   response.json({ mensagem: "Servidor Retornando" });
 });
 
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
 // conectando ao mongoDB
 mongoose
   .connect(
-    "mongodb+srv://alison:IZsgfuwOGmTG1hz4@apicluster.g3ewu.mongodb.net/bdApi?retryWrites=true&w=majority"
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.g3ewu.mongodb.net/bdApi?retryWrites=true&w=majority`
   ) // promisse
   .then(() => {
     console.log("Conectamos ao mongoDB");
